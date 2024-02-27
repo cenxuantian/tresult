ffaf60d36aff0a0a5c7f85fd70e0edf452ecb7b4
Cenxuan Tian <cenxuan.tian@outlook.com>
2024-02-27 22:10:02 +0800

    update(Result.hpp):add license & macro

--- Result.hpp
+++ Result.hpp
@@ -1,4 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2024 Cenxuan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #pragma once
+#ifndef __TRESULT_RESULT_HPP__
+#define __TRESULT_RESULT_HPP__
 #include <type_traits>
 #include <functional>
 #include <algorithm>
@@ -291,3 +318,6 @@ public:
 
 
 };
+
+
+#endif
